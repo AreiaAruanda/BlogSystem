ea6a7323e79f9fec9f82f2c88544736c3d961b72
AreiaAruanda <144889202+AreiaAruanda@users.noreply.github.com>
2024-01-17 17:06:44 +0100

    First commit

---
+++ Model.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+public class BloggingContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<User> Users { get; set; }
+    public string DbPath { get; }
+
+    public BloggingContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "blogging2.db");
+        //var folder = Environment.CurrentDirectory;
+        //DbPath = System.IO.Path.Join(folder, "blogging3.db");
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Blog>()
+            .HasMany(b => b.Posts)
+            .WithOne(b => b.Blog)
+            .HasForeignKey(b => b.BlogId)
+            .HasPrincipalKey(b => b.Id);
+
+        modelBuilder.Entity<User>()
+           .HasMany(u => u.Posts)
+           .WithOne(u => u.User)
+           .HasForeignKey(u => u.UserId)
+           .HasPrincipalKey(u => u.Id);
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
+
+public class User
+{
+    public int Id { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+    public List<Post>? Posts { get; set; } = new();
+}
+public class Blog
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Url { get; set; }
+    public List<Post>? Posts { get; set; } = new();
+}
+
+public class Post
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Content { get; set; }
+    public int BlogId { get; set; }
+    public int UserId { get; set; }
+
+    public Blog? Blog { get; set; }
+    public User? User { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Threading.Channels;
+
+using var db = new BloggingContext();
+
+Console.WriteLine($"Database path: {db.DbPath}.");
+
+//Deleting table content 
+#region
+db.Users.RemoveRange(db.Users);
+db.Posts.RemoveRange(db.Posts);
+db.Blogs.RemoveRange(db.Blogs);
+db.SaveChanges();
+#endregion
+
+//creating correct file paths
+string projectDirectory = @"C:\Users\lipsk\source\repos\BlogSystem\";
+//Console.WriteLine($"Project Directory: {projectDirectory}");
+
+string csvFilePathUser = Path.Combine(Directory.GetParent(projectDirectory).FullName, "User.csv");
+string csvFilePathBlog = Path.Combine(Directory.GetParent(projectDirectory).FullName, "Blog.csv");
+string csvFilePathPost = Path.Combine(Directory.GetParent(projectDirectory).FullName, "Post.csv");
+
+// Reading data from CSV files
+var users = ReadUserFromCsv(csvFilePathUser);
+var blogs = ReadBlogFromCsv(csvFilePathBlog);
+var posts = ReadPostFromCsv(csvFilePathPost);
+
+// Adding data to db
+db.Users.AddRange(users);
+db.Blogs.AddRange(blogs);
+db.Posts.AddRange(posts);
+
+db.SaveChanges();
+
+// Displaying the data
+DisplayData(db);
+
+//Read from CSVs
+#region
+static List<User> ReadUserFromCsv(string filePath)
+{
+    var users = new List<User>();
+    var processedIds = new List<int>(); //tracks if user already exists
+    var lines = File.ReadAllLines(filePath);
+
+    foreach (var line in lines)
+    {
+        var parts = line.Split(",");
+        if (int.TryParse(parts[0], out int id) && !processedIds.Contains(id))
+        {
+            users.Add(new User
+            {
+                Id = id,
+                Username = parts[1],
+                Password = parts[2],
+            });
+            processedIds.Add(id);
+        }
+        else
+        {
+            continue;
+        }
+    }
+    return users;           
+}
+db.SaveChanges();
+
+static List<Blog> ReadBlogFromCsv(string filePath)
+{
+    var processedIds = new List<int>(); //tracks if blog already exists
+    var blogs = new List<Blog>();
+    var lines = File.ReadAllLines(filePath); 
+
+    foreach (var line in lines)
+    {
+        var parts = line.Split(",");
+        if (int.TryParse(parts[0], out int id) && !processedIds.Contains(id))
+        {
+            blogs.Add(new Blog
+            {
+                Id = id,
+                Name = parts[1],
+                Url = parts[2]
+            });
+            processedIds.Add(id);
+        }
+        else
+        {
+            continue;
+        }
+    }
+    return blogs;
+}
+db.SaveChanges();
+
+static List<Post> ReadPostFromCsv(string filePath)
+{
+    var posts = new List<Post>();
+    var lines = File.ReadAllLines(filePath); 
+
+    foreach (var line in lines)
+    {
+        var parts = line.Split(",");
+        posts.Add(new Post
+        {
+            Id = Convert.ToInt32(parts[0]),
+            Title = parts[1],
+            Content = parts[2],
+            BlogId = Convert.ToInt32(parts[3]),
+            UserId = Convert.ToInt32(parts[4])
+        });
+    }
+    return posts;
+}
+db.SaveChanges();
+#endregion
+
+//Output
+static void DisplayData(BloggingContext db)
+{
+    Console.WriteLine();
+    Console.WriteLine("\u001b[1mUSERS:\u001b[0m");
+    foreach (var user in db.Users)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"    \u001b[1mUserID:{user.Id}\u001b[0m");
+        Console.WriteLine($"        Username: {user.Username}");
+        Console.WriteLine($"        Password: {user.Password}");
+        Console.WriteLine($"        Posts:");
+        foreach (Post post in user.Posts)
+        {
+            Console.WriteLine($"            - {post.Title}");
+        }
+    }
+
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine("\u001b[1mBLOGS:\u001b[0m");
+    foreach (var blog in db.Blogs)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"    \u001b[1mBlogID:{blog.Id}\u001b[0m");
+        Console.WriteLine($"        Name: {blog.Name}");
+        Console.WriteLine($"        URL: {blog.Url}");
+        Console.WriteLine($"        Posts:");
+        foreach (Post post in blog.Posts)
+        {
+            Console.WriteLine($"            - {post.Title}");
+        }
+    }
+
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine("\u001b[1mPOSTS:\u001b[0m");
+    Console.WriteLine();
+
+    foreach (var post in db.Posts)
+    {
+        Console.WriteLine("     \u001b[1mPostID:" + post.Id + "\u001b[0m");
+        Console.WriteLine($"         Title: {post.Title}");
+        Console.WriteLine($"         Content: {post.Content}");
+        Console.WriteLine($"         Published on '{post.Blog.Name}' (BlogID: {post.BlogId})");
+        Console.WriteLine($"         By '{post.User.Username}' (UserID: {post.UserId})");
+        Console.WriteLine();
+    }
+}
